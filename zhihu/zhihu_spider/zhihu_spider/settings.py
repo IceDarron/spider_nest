44f119706e2dd9cc8cce8bfdc4a04835d0e3255a
IceDarron <darron_r@163.com>
2018-01-28 21:30:04 +0800

    Update settings.py

--- zhihu/zhihu_spider/zhihu_spider/settings.py
+++ zhihu/zhihu_spider/zhihu_spider/settings.py
@@ -67,6 +67,9 @@ ROBOTSTXT_OBEY = True
 #ITEM_PIPELINES = {
 #    'zhihu_spider.pipelines.ZhihuSpiderPipeline': 300,
 #}
+ITEM_PIPELINES = {
+    'zhihu_spider.pipelines.ZhihuSpiderPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
