d04fc88dcb761e0a7f127bef481ad07015ee2480
IceDarron <darron_r@163.com>
2018-01-30 00:04:57 +0800

    整理代码格式

--- zhihu/zhihu_spider/spiders/users.py
+++ zhihu/zhihu_spider/spiders/users.py
@@ -3,7 +3,8 @@ import scrapy
 import os
 import time
 from zhihu_spider.items import ZhihuSpiderItem
-from zhihu_spider.myconfig import UsersConfig # 爬虫配置
+from zhihu_spider.myconfig import UsersConfig  # 爬虫配置
+
 
 class UsersSpider(scrapy.Spider):
     name = 'users'
@@ -17,21 +18,21 @@ class UsersSpider(scrapy.Spider):
         "Upgrade-Insecure-Requests": "1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"
     }
- 
-    def __init__(self, url = None):
+
+    def __init__(self, url=None):
         self.user_url = url
- 
+
     def start_requests(self):
         yield scrapy.Request(
-            url = self.domain,
-            headers = self.headers,
-            meta = {
+            url=self.domain,
+            headers=self.headers,
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': 1
             },
-            callback = self.request_captcha
+            callback=self.request_captcha
         )
- 
+
     def request_captcha(self, response):
         # 获取_xsrf值
         _xsrf = response.css('input[name="_xsrf"]::attr(value)').extract()[0]
@@ -39,16 +40,16 @@ class UsersSpider(scrapy.Spider):
         captcha_url = 'http://www.zhihu.com/captcha.gif?r=' + str(time.time() * 1000)
         # 准备下载验证码
         yield scrapy.Request(
-            url = captcha_url,
-            headers = self.headers,
-            meta = {
+            url=captcha_url,
+            headers=self.headers,
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': response.meta['cookiejar'],
                 '_xsrf': _xsrf
             },
-            callback = self.download_captcha
+            callback=self.download_captcha
         )
- 
+
     def download_captcha(self, response):
         # 下载验证码
         with open('captcha.gif', 'wb') as fp:
@@ -56,31 +57,32 @@ class UsersSpider(scrapy.Spider):
         # 用软件打开验证码图片
         os.system('start captcha.gif')
         # 输入验证码
-        print 'Please enter captcha: '
-        captcha = raw_input()
- 
+        print
+        'Please enter captcha: '
+        captcha = input()
+
         yield scrapy.FormRequest(
-            url = self.login_url,
-            headers = self.headers,
-            formdata = {
+            url=self.login_url,
+            headers=self.headers,
+            formdata={
                 'email': UsersConfig['email'],
                 'password': UsersConfig['password'],
                 '_xsrf': response.meta['_xsrf'],
                 'remember_me': 'true',
                 'captcha': captcha
             },
-            meta = {
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': response.meta['cookiejar']
             },
-            callback = self.request_zhihu
+            callback=self.request_zhihu
         )
- 
+
     def request_zhihu(self, response):
         yield scrapy.Request(
-            url = self.user_url + '/about',
-            headers = self.headers,
-            meta = {
+            url=self.user_url + '/about',
+            headers=self.headers,
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': response.meta['cookiejar'],
                 'from': {
@@ -88,14 +90,14 @@ class UsersSpider(scrapy.Spider):
                     'data': {}
                 }
             },
-            callback = self.user_item,
-            dont_filter = True
+            callback=self.user_item,
+            dont_filter=True
         )
- 
+
         yield scrapy.Request(
-            url = self.user_url + '/followees',
-            headers = self.headers,
-            meta = {
+            url=self.user_url + '/followees',
+            headers=self.headers,
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': response.meta['cookiejar'],
                 'from': {
@@ -103,14 +105,14 @@ class UsersSpider(scrapy.Spider):
                     'data': {}
                 }
             },
-            callback = self.user_start,
-            dont_filter = True
+            callback=self.user_start,
+            dont_filter=True
         )
- 
+
         yield scrapy.Request(
-            url = self.user_url + '/followers',
-            headers = self.headers,
-            meta = {
+            url=self.user_url + '/followers',
+            headers=self.headers,
+            meta={
                 'proxy': UsersConfig['proxy'],
                 'cookiejar': response.meta['cookiejar'],
                 'from': {
@@ -118,21 +120,21 @@ class UsersSpider(scrapy.Spider):
                     'data': {}
                 }
             },
-            callback = self.user_start,
-            dont_filter = True
+            callback=self.user_start,
+            dont_filter=True
         )
- 
+
     def user_start(self, response):
         sel_root = response.xpath('//h2[@class="zm-list-content-title"]')
         # 判断关注列表是否为空
         if len(sel_root):
             for sel in sel_root:
                 people_url = sel.xpath('a/@href').extract()[0]
- 
+
                 yield scrapy.Request(
-                    url = people_url + '/about',
-                    headers = self.headers,
-                    meta = {
+                    url=people_url + '/about',
+                    headers=self.headers,
+                    meta={
                         'proxy': UsersConfig['proxy'],
                         'cookiejar': response.meta['cookiejar'],
                         'from': {
@@ -140,14 +142,14 @@ class UsersSpider(scrapy.Spider):
                             'data': {}
                         }
                     },
-                    callback = self.user_item,
-                    dont_filter = True
+                    callback=self.user_item,
+                    dont_filter=True
                 )
- 
+
                 yield scrapy.Request(
-                    url = people_url + '/followees',
-                    headers = self.headers,
-                    meta = {
+                    url=people_url + '/followees',
+                    headers=self.headers,
+                    meta={
                         'proxy': UsersConfig['proxy'],
                         'cookiejar': response.meta['cookiejar'],
                         'from': {
@@ -155,14 +157,14 @@ class UsersSpider(scrapy.Spider):
                             'data': {}
                         }
                     },
-                    callback = self.user_start,
-                    dont_filter = True
+                    callback=self.user_start,
+                    dont_filter=True
                 )
- 
+
                 yield scrapy.Request(
-                    url = people_url + '/followers',
-                    headers = self.headers,
-                    meta = {
+                    url=people_url + '/followers',
+                    headers=self.headers,
+                    meta={
                         'proxy': UsersConfig['proxy'],
                         'cookiejar': response.meta['cookiejar'],
                         'from': {
@@ -170,16 +172,16 @@ class UsersSpider(scrapy.Spider):
                             'data': {}
                         }
                     },
-                    callback = self.user_start,
-                    dont_filter = True
+                    callback=self.user_start,
+                    dont_filter=True
                 )
- 
+
     def user_item(self, response):
         def value(list):
             return list[0] if len(list) else ''
- 
+
         sel = response.xpath('//div[@class="zm-profile-header ProfileCard"]')
- 
+
         item = ZhihuSpiderItem()
         item['url'] = response.url[:-6]
         item['name'] = sel.xpath('//a[@class="name"]/text()').extract()[0].encode('utf-8')
@@ -193,9 +195,11 @@ class UsersSpider(scrapy.Spider):
         item['employment'] = value(sel.xpath('//span[contains(@class, "employment")]/@title').extract()).encode('utf-8')
         item['position'] = value(sel.xpath('//span[contains(@class, "position")]/@title').extract()).encode('utf-8')
         item['content'] = value(sel.xpath('//span[@class="content"]/text()').extract()).strip().encode('utf-8')
-        item['ask'] = int(sel.xpath('//div[contains(@class, "profile-navbar")]/a[2]/span[@class="num"]/text()').extract()[0])
-        item['answer'] = int(sel.xpath('//div[contains(@class, "profile-navbar")]/a[3]/span[@class="num"]/text()').extract()[0])
+        item['ask'] = int(
+            sel.xpath('//div[contains(@class, "profile-navbar")]/a[2]/span[@class="num"]/text()').extract()[0])
+        item['answer'] = int(
+            sel.xpath('//div[contains(@class, "profile-navbar")]/a[3]/span[@class="num"]/text()').extract()[0])
         item['agree'] = int(sel.xpath('//span[@class="zm-profile-header-user-agree"]/strong/text()').extract()[0])
         item['thanks'] = int(sel.xpath('//span[@class="zm-profile-header-user-thanks"]/strong/text()').extract()[0])
- 
+
         yield item
