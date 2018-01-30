cb879d161f4623e95e0b4304ab063ed27e6b3ee6
IceDarron <darron_r@163.com>
2018-01-30 23:56:36 +0800

    增加数据库 时间 配置文件包

--- zhihu/zhihu_spider/pipelines.py
+++ zhihu/zhihu_spider/pipelines.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import MySQLdb
+import datetime
+from zhihu_spider.myconfig import DbConfig
 
 
 class ZhihuSpiderPipeline(object):
     def __init__(self):
-        self.conn = MySQLdb.connect(user = DbConfig['user'], passwd = DbConfig['passwd'], db = DbConfig['db'], host = DbConfig['host'], charset = 'utf8', use_unicode = True)
+        self.conn = MySQLdb.connect(user=DbConfig['user'], passwd=DbConfig['passwd'], db=DbConfig['db'],
+                                    host=DbConfig['host'], charset='utf8', use_unicode=True)
         self.cursor = self.conn.cursor()
         # 清空表
         # self.cursor.execute('truncate table weather;')
         # self.conn.commit()
- 
+
     def process_item(self, item, spider):
         curTime = datetime.datetime.now()
         try:
@@ -41,7 +40,7 @@ class ZhihuSpiderPipeline(object):
                 )
             )
             self.conn.commit()
-        except MySQLdb.Error, e:
-            print 'Error %d %s' % (e.args[0], e.args[1])
- 
+        except MySQLdb.Error as e:
+            print('Error %d %s' % (e.args[0], e.args[1]))
+
         return item
